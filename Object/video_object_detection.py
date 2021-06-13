68dfdfe0217c7a2e413fc29dbc9dddb53fde088f
jithu <jithushaji23@gmail.com>
2021-06-13 21:02:50 +0530

    updated

--- Object/video_object_detection.py
+++ Object/video_object_detection.py
@@ -6,92 +6,90 @@ import imutils
 import time
 import cv2
 
+def object_rec():
 # construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
+    ap = argparse.ArgumentParser()
 
-# ap.add_argument('-p', '--prototxt', default='/Users/siddhantbansal/Desktop/Python/Personal_Projects/Object_Detection/MobileNetSSD_deploy.prototxt.txt', help='path to Caffe deploy prototxt file')
-# ap.add_argument('-m', '--model', default='/Users/siddhantbansal/Desktop/Python/Personal_Projects/Object_Detection/MobileNetSSD_deploy.caffemodel', help='path to the Caffe pre-trained model')
-ap.add_argument('-p', '--prototxt', default="MobileNetSSD_deploy.prototxt.txt", help='path to Caffe deploy prototxt file')
-ap.add_argument('-m', '--model', default="MobileNetSSD_deploy.caffemodel", help='path to the Caffe pre-trained model')
-ap.add_argument('-c', '--confidence', type=float, default=0.7, help='minimum probability to filter weak detections')
-args = vars(ap.parse_args())
+    # ap.add_argument('-p', '--prototxt', default='/Users/siddhantbansal/Desktop/Python/Personal_Projects/Object_Detection/MobileNetSSD_deploy.prototxt.txt', help='path to Caffe deploy prototxt file')
+    # ap.add_argument('-m', '--model', default='/Users/siddhantbansal/Desktop/Python/Personal_Projects/Object_Detection/MobileNetSSD_deploy.caffemodel', help='path to the Caffe pre-trained model')
+    ap.add_argument('-p', '--prototxt', default="MobileNetSSD_deploy.prototxt.txt", help='path to Caffe deploy prototxt file')
+    ap.add_argument('-m', '--model', default="MobileNetSSD_deploy.caffemodel", help='path to the Caffe pre-trained model')
+    ap.add_argument('-c', '--confidence', type=float, default=0.7, help='minimum probability to filter weak detections')
+    args = vars(ap.parse_args())
 
-# initialize the list of class labels MobileNet SSD was trained to
-# detect, then generate a set of bounding box colors for each class
-CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
-           "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-           "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-           "sofa", "train", "tvmonitor"]
-COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+    # initialize the list of class labels MobileNet SSD was trained to
+    # detect, then generate a set of bounding box colors for each class
+    CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+               "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+               "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+               "sofa", "train", "tvmonitor"]
+    COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
-# load our serialized model from disk
-print("[INFO] loading model...")
-net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+    # load our serialized model from disk
+    print("[INFO] loading model...")
+    net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
-# initialze the video stream, allow the camera to sensor to warmup,
-# and initlaize the FPS counter
-print('[INFO] starting video stream...')
-vs = VideoStream(src=0).start()
-time.sleep(2.0)
-fps = FPS().start()
+    # initialze the video stream, allow the camera to sensor to warmup,
+    # and initlaize the FPS counter
+    print('[INFO] starting video stream...')
+    vs = VideoStream(src=0).start()
+    time.sleep(2.0)
 
-t_end = time.time() + 5 #stay in the loop for 5 seconds
-    
-# loop over the frames from the video stream
-while (time.time() < t_end):
-    # grab the frame from the threadeed video stream and resize it
-    # to have a max width of 400 pixels
-    frame = vs.read()
-    frame = imutils.resize(frame, width=400)
 
-    # grab the frame dimentions and convert it to a blob
-    (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
+    t_end = time.time() + 5 #stay in the loop for 5 seconds
+        
+    # loop over the frames from the video stream
+    while (time.time() < t_end):
+        # grab the frame from the threadeed video stream and resize it
+        # to have a max width of 400 pixels
+        frame = vs.read()
+        frame = imutils.resize(frame, width=400)
 
-    # pass the blob through the network and obtain the detections and predictions
-    net.setInput(blob)
-    detections = net.forward()
+        # grab the frame dimentions and convert it to a blob
+        (h, w) = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
 
-    # loop over the detections
-    for i in np.arange(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with
-        # the prediction
-        confidence = detections[0, 0, i, 2]
+        # pass the blob through the network and obtain the detections and predictions
+        net.setInput(blob)
+        detections = net.forward()
 
-        # filter out weak detections by ensuring the `confidence` is
-        # greater than the minimum confidence
-        if confidence > args["confidence"]:
-            # extract the index of the class label from the
-            # `detections`, then compute the (x, y)-coordinates of
-            # the bounding box for the object
-            idx = int(detections[0, 0, i, 1])
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+        # loop over the detections
+        for i in np.arange(0, detections.shape[2]):
+            # extract the confidence (i.e., probability) associated with
+            # the prediction
+            confidence = detections[0, 0, i, 2]
 
-            # draw the prediction on the frame
-            label = "{}: {:.2f}%".format(CLASSES[idx],
-                                         confidence * 100)
-            cv2.rectangle(frame, (startX, startY), (endX, endY),
-                          COLORS[idx], 2)
-            y = startY - 15 if startY - 15 > 15 else startY + 15
-            cv2.putText(frame, label, (startX, y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
-    # show the output frame
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
+            # filter out weak detections by ensuring the `confidence` is
+            # greater than the minimum confidence
+            if confidence > args["confidence"]:
+                # extract the index of the class label from the
+                # `detections`, then compute the (x, y)-coordinates of
+                # the bounding box for the object
+                idx = int(detections[0, 0, i, 1])
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
 
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
+                # draw the prediction on the frame
+                label = "{}: {:.2f}%".format(CLASSES[idx],
+                                             confidence * 100)
+                cv2.rectangle(frame, (startX, startY), (endX, endY),
+                              COLORS[idx], 2)
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+                cv2.putText(frame, label, (startX, y),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+        # show the output frame
+        cv2.imshow("Frame", frame)        
+        key = cv2.waitKey(1) & 0xFF
+        print(label)
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
 
-    # update the FPS counter
-    fps.update()
+        # update the FPS countqer
 
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    vs.stop()
 
-# do a bit of cleanup
-cv2.destroyAllWindows()
-vs.stop()
+
+object_rec()
